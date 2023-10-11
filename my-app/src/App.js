7f73e0fecbcc56f3bbe64e578e4fea244dc0ec7a
edalonzoh90 <edalonzoh@gmail.com>
2023-10-11 17:10:44 -0600

    Events

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
-import Expenses from  './components/Expenses';
+import Expenses from  './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
 
@@ -8,8 +9,16 @@ function App() {
     {id: 'e3', title: 'Title 3', amount: 300, date: new Date(2023,9,27)},
     {id: 'e4', title: 'Title 4', amount: 400, date: new Date(2023,9,28)},
   ];
+
+  const addExpenseHandler = expense => {
+    console.log("expense");
+    console.log(expense);
+  }
+
   return (
+    
     <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
       
     </div>
